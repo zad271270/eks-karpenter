f2ab50f8f5c914b0c42ffd17d10b85ab4b1c63d8
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-03-03 15:18:12 +0500

    Update main.tf

--- terraform/environment/dev/main.tf
+++ terraform/environment/dev/main.tf
@@ -117,8 +117,8 @@ data "aws_eks_cluster_auth" "eks" {
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
