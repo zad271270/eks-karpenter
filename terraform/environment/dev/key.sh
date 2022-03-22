aac6ab79fa99141883ea55b6567dd7e9bb310429
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-03-22 09:58:19 +0000

    Create key.sh

---
+++ terraform/environment/dev/key.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -d /root/.ssh ]
+then
+echo "/root/.ssh exists"
+else
+mkdir -p /root/.ssh
+fi
+
+if [ -f /root/.ssh/*.pem ]
+then
+echo "pem is there, I am removing it"
+rm -f ~/.ssh/*.pem
+export SSH_KEY_NAME="terraform-cloudgeeks-eks"
+aws ec2 create-key-pair --key-name "${SSH_KEY_NAME}" --query 'KeyMaterial' --output text > ~/.ssh/${SSH_KEY_NAME}.pem
+else
+echo "All is well, now I am creating fresh PEM"
+export SSH_KEY_NAME="terraform-cloudgeeks"
+aws ec2 create-key-pair --key-name "${SSH_KEY_NAME}" --query 'KeyMaterial' --output text > ~/.ssh/${SSH_KEY_NAME}.pem
+fi
+
+#End
