2b262c690ef4608322a1dc49c2b645e5583a9e50
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-03-23 11:41:26 +0000

    key name updated

--- terraform/environment/dev/key.sh
+++ terraform/environment/dev/key.sh
@@ -19,7 +19,7 @@ if [ -f /root/.ssh/*.pem ]
 then
 echo "pem is there, I am removing it"
 rm -f ~/.ssh/*.pem
-export SSH_KEY_NAME="terraform-cloudgeeks-eks"
+export SSH_KEY_NAME="terraform-cloudgeeks"
 aws ec2 create-key-pair --key-name "${SSH_KEY_NAME}" --query 'KeyMaterial' --output text > ~/.ssh/${SSH_KEY_NAME}.pem
 else
 echo "All is well, now I am creating fresh PEM"
