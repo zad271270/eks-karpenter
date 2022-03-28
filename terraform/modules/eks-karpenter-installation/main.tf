44e056edae2fa1348796fdadb5a8810e0c3f4105
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-03-28 06:20:33 +0500

    Update main.tf

--- terraform/modules/eks-karpenter-installation/main.tf
+++ terraform/modules/eks-karpenter-installation/main.tf
@@ -6,7 +6,7 @@ resource "helm_release" "karpenter" {
   name       = "karpenter"
   repository = "https://charts.karpenter.sh"
   chart      = "karpenter"
-  version    = "0.5.3"
+  version    = var.karpenter_version
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
