a095a23b0fe692f24b6686209c6a2a6ba1ccbe12
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-04-11 07:26:40 +0000

    Update karpenter.sh

--- karpenter.sh
+++ karpenter.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# https://cmp307.ec2spotworkshops.com/040_karpenter/install_karpenter.html
 # https://karpenter.sh/v0.8.1/getting-started/getting-started-with-eksctl/
 
 
@@ -7,21 +6,23 @@ export CLUSTER_NAME="cloudgeeks-eks-dev"
 export CLUSTER_ENDPOINT="$(aws eks describe-cluster --name ${CLUSTER_NAME} --query "cluster.endpoint" --output text)"
 export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
 export KARPENTER_IAM_ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/karpenter-controller-${CLUSTER_NAME}"
+export KARPENTER_VERSION=v0.8.1
 
 echo $CLUSTER_NAME
 echo $CLUSTER_ENDPOINT
 echo $KARPENTER_IAM_ROLE_ARN
+echo $KARPENTER_VERSION
 
-helm repo add karpenter https://charts.karpenter.sh
+helm repo add karpenter https://charts.karpenter.sh/
 helm repo update
-helm upgrade --install karpenter karpenter/karpenter --namespace karpenter \
-  --create-namespace --version 0.8.1 \
+helm upgrade --install --namespace karpenter --create-namespace \
+  karpenter karpenter/karpenter \
+  --version ${KARPENTER_VERSION} \
   --set serviceAccount.annotations."eks\.amazonaws\.com/role-arn"=${KARPENTER_IAM_ROLE_ARN} \
   --set clusterName=${CLUSTER_NAME} \
   --set clusterEndpoint=${CLUSTER_ENDPOINT} \
   --set aws.defaultInstanceProfile=KarpenterNodeInstanceProfile-${CLUSTER_NAME} \
   --wait # for the defaulting webhook to install before creating a Provisioner
-
 
  # End 
 
