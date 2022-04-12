49c4c5cf7fe9209fa9847ce0c1f2cc9eae54b312
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-04-12 11:34:21 +0000

    Update karpenter.sh

--- karpenter.sh
+++ karpenter.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 # https://karpenter.sh/v0.8.2/getting-started/getting-started-with-eksctl/
+# # https://karpenter.sh/v0.8.2/getting-started/migrating-from-cas/
 
 
 export CLUSTER_NAME="cloudgeeks-eks-dev"
+export VPC_ID="vpc-0b4e0e96e579a2170"
+export REGION="us-east-1"
 export CLUSTER_ENDPOINT="$(aws eks describe-cluster --name ${CLUSTER_NAME} --query "cluster.endpoint" --output text)"
 export AWS_ACCOUNT_ID="$(aws sts get-caller-identity --query Account --output text)"
 export KARPENTER_IAM_ROLE_ARN="arn:aws:iam::${AWS_ACCOUNT_ID}:role/karpenter-controller-${CLUSTER_NAME}"
 export KARPENTER_VERSION=v0.8.2
+
+
+
+SECURITY_GROUPS=$(aws ec2 describe-security-groups --region $REGION --filter Name=vpc-id,Values=${VPC_ID} Name=group-name,Values=${CLUSTER_NAME}* --query 'SecurityGroups[*].[GroupId]' --output text)
+
+export SECURITY_GROUPS
+echo $SECURITY_GROUPS
+
+aws ec2 create-tags \
+    --tags "Key=karpenter.sh/discovery,Value=${CLUSTER_NAME}" \
+    --resources ${SECURITY_GROUPS}
+
 
 echo $CLUSTER_NAME
 echo $CLUSTER_ENDPOINT
